adaade06c5e6c3131376e75d528c432dfc4c8d1f
SaloniSS <sxs170015@utdallas.edu>
2021-02-27 23:31:14 -0600

    public and private channels work

--- commands/studyGroup.js
+++ commands/studyGroup.js
@@ -5,18 +5,26 @@ const Discord = require("discord.js");
 
 exports.studyGroup = async (msg, content) => {
   msg.react("🤓");
+  groupName = content.substring(content.indexOf(" ") + 1);
+  var role = await msg.guild.roles.create({
+    data: { name: groupName, mentionable: true },
+  });
+  var permissions = [];
   if (content.startsWith("public")) {
     //do public stuff
-    console.log(content.substring(content.indexOf(" ") + 1));
+    permissions = [
+      {
+        id: msg.guild.id,
+        allow: ["VIEW_CHANNEL"],
+        type: "role",
+      },
+    ];
+    console.log("public");
   }
   if (content.startsWith("private")) {
     //do private stuff
-    groupName = content.substring(content.indexOf(" ") + 1);
-    var role = await msg.guild.roles.create({
-      data: { name: groupName, mentionable: true },
-    });
-    console.log(role);
-    var permissions = [
+    console.log("private");
+    permissions = [
       {
         id: msg.guild.id,
         deny: ["VIEW_CHANNEL"],
@@ -28,19 +36,19 @@ exports.studyGroup = async (msg, content) => {
         type: "role",
       },
     ];
-    var groupCategory = await msg.guild.channels.create(groupName, {
-      type: "category",
-      permissionOverwrites: permissions,
-    });
-    var groupChannel = await msg.guild.channels.create(groupName, {
-      type: "text",
-      parent: groupCategory,
-      permissionOverwrites: permissions,
-    });
-    var groupVoiceChannel = await msg.guild.channels.create(groupName + " VC", {
-      type: "voice",
-      parent: groupCategory,
-      permissionOverwrites: permissions,
-    });
   }
+  var groupCategory = await msg.guild.channels.create(groupName, {
+    type: "category",
+    permissionOverwrites: permissions,
+  });
+  var groupChannel = await msg.guild.channels.create(groupName, {
+    type: "text",
+    parent: groupCategory,
+    permissionOverwrites: permissions,
+  });
+  var groupVoiceChannel = await msg.guild.channels.create(groupName + " VC", {
+    type: "voice",
+    parent: groupCategory,
+    permissionOverwrites: permissions,
+  });
 };
