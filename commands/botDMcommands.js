b7a003174d8aebda11f2e114393f1c4d6124dad4
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2021-02-28 00:33:29 -0600

    Update botDMcommands.js

--- commands/botDMcommands.js
+++ commands/botDMcommands.js
@@ -2,15 +2,15 @@ const Discord = require("discord.js");
 const fakeData = [
     {
         name: "Assignment 1",
-        value: "11/25/21"
+        value: ["10/20/21", "submitted"]
     },
     {
         name: "Assignment 2",
-        value: "12/24/21"
+        value: ["11/29/21", "not submitted"]
     },
     {
         name: "Assignment 3",
-        value: "10/20/21"
+        value: ["12/24/21", "submitted"]
     },
 ];
 
@@ -19,7 +19,8 @@ const stringData = fakeData.map(info => `${info.name} ${info.date}`)
 exports.getHistory = async (msg) => {
   const embed = new Discord.MessageEmbed()
     .setTitle("Assignment History:")
-    .setColor(0xffffff)
+    .setColor(0x0)
+    .setDescription("Here is a history of assignments and their due dates.")
     .addFields(fakeData);
 
     msg.author.send(embed);
