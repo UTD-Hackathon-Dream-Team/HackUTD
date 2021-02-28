9d29dbddd21e2dc01a0a924ed60a3d40dd215c7a
SaloniSS <sxs170015@utdallas.edu>
2021-02-27 23:10:49 -0600

    create category

--- commands/studyGroup.js
+++ commands/studyGroup.js
@@ -11,6 +11,23 @@ exports.studyGroup = async (msg, content) => {
   }
   if (content.startsWith("private")) {
     //do private stuff
-    console.log(content.substring(content.indexOf(" ") + 1));
+    groupName = content.substring(content.indexOf(" ") + 1);
+    var role = await msg.guild.roles.create({
+      data: { name: groupName, mentionable: true },
+    });
+    var permissions = [
+      {
+        id: role,
+        allow: ["VIEW_CHANNEL"],
+        type: "role",
+      },
+    ];
+    var projectCategory = await msg.guild.channels.create("test", {
+      type: "category",
+    });
+    // var projectCategory = await msg.guild.channels.create(role, {
+    //   type: "category",
+    //   permissionOverwrites: permissions,
+    // });
   }
 };
