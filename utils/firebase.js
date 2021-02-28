e1d29e1d6d1a2b0e9dabb8c607f2758febfdc194
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2021-02-28 02:23:27 -0600

    aaaaaaaaaaaaaaaaaaaaaAAAAAAAAAAAAAAhhhhhhhh

--- utils/firebase.js
+++ utils/firebase.js
@@ -13,3 +13,17 @@ const firebase = require("firebase");
 require("firebase/firestore");
 firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
+
+exports.GETass = async (id) => {
+  var assignments = [];
+  await db
+    .collection("users")
+    .doc(id)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((field) => {
+        assignments = field.data();
+      });
+    }); 
+  return assignments;
+};
